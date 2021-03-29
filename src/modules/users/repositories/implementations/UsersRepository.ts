f205c2853e98ae4f67eb58f03d7fb77a3b1ca92a
Paulo Vieira <paullocostta86@gmail.com>
2021-03-29 01:20:17 -0300

    ✅ refactor: complete repository methods in UsersRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -13,18 +13,28 @@ export class UsersRepository implements IUsersRepository {
 
   async findUserWithGamesById({
     user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  }: IFindUserWithGamesDTO): Promise<User | undefined> {
+    const user = await this.repository.findOne({
+      where: {
+        id: user_id
+      },
+      relations: ['games']
+    });
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    const firstName = first_name.toLowerCase();
+    const lastName = last_name.toLowerCase();
+
+    return this.repository
+      .query(`SELECT * FROM users WHERE Lower(first_name) = '${firstName}' AND Lower(last_name) = '${lastName}'`);
   }
 }
