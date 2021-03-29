8a8407d7f55535d9d914ad2f60c2937560a63ece
Paulo Vieira <paullocostta86@gmail.com>
2021-03-29 03:17:16 -0300

    ✅ feat: create orders migrations

---
+++ src/database/migrations/1616993082338-CreateUserOrders.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class CreateUserOrders1616993082338 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+
+          { name: 'user_id', type: 'uuid' },
+          { name: 'game_id', type: 'uuid' },
+
+          { name: 'amount', type: 'number' },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+            isNullable: false,
+          },
+          { name: 'purchased', type: 'boolean', default: false },
+
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+          { name: 'updated_at', type: 'timestamp', default: 'now()' },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        name: 'fk_users_orders',
+        onDelete: 'CASCADE',
+        onUpdate: 'SET NULL',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        columnNames: ['game_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'games',
+        name: 'fk_games_orders',
+        onDelete: 'CASCADE',
+        onUpdate: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders',
+      'fk_games_orders',
+    );
+
+    await queryRunner.dropForeignKey(
+      'orders',
+      'fk_users_orders',
+    );
+
+    await queryRunner.dropTable('orders');
+  }
+}
